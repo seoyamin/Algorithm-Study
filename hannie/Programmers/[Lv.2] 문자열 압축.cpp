dca4e94fa175f8d34007043ee8f3386df3eeb499
Hannie Shin <50253860+hannie0615@users.noreply.github.com>
2022-07-26 08:11:59 +0900

    [Week 3] 문자열 압축

--- hannie/Programmers/[Lv.2] 문자열 압축.cpp
+++ hannie/Programmers/[Lv.2] 문자열 압축.cpp
@@ -7,7 +7,7 @@ int solution(string s) {
     int answer=s.size();
 
     for(int i=1;i<=s.size()/2;i++){ // 1/2씩 나눠가기
-        string convert,temp;
+        string convert,temp; // 문자열
 
         int cnt=1;
         temp=s.substr(0,i); // i까지 문자열 자르기
