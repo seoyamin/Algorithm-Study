6098c783df0727907e53a3200f85779244fb44a5
Hannie Shin <50253860+hannie0615@users.noreply.github.com>
2022-10-16 19:30:21 +0900

    Create [Lv.1] 하샤드 수.cpp

---
+++ hannie/Programmers/[Lv.1] 하샤드 수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    string s = to_string(x);
+    int sum=0;
+    for(char c:s)
+        sum+=(c-'0');
+    return x%sum==0;
+}
