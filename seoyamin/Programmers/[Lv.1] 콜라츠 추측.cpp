3991aae4fcc7457f4fbb831b3823f8acff97f6da
seoyamin <seoyamin@ewhain.net>
2023-07-10 23:56:22 +0900

    [Week 53] 콜라츠 추측

---
+++ seoyamin/Programmers/[Lv.1] 콜라츠 추측.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int count = 0;
+    long numL = num;
+    
+    while(numL != 1) {
+        if(count == 500) return -1;
+        
+        if(numL % 2 == 0) numL /= 2;
+        else numL = numL * 3 + 1;
+        count++;
+    }
+    
+    return count;
+}
